46a6c6dabb5c317fa4ff2b18be44596c44e21e83
truebigsand <3244346642@qq.com>
2023-01-24 13:26:40 +0800

    Update: remove supabase

--- main.js
+++ main.js
@@ -16,10 +16,3 @@ const router = VueRouter.createRouter({
 })
 
 createApp(App).use(router).mount('#app')
-
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
